9da94d2d8079a3f6be1b4cc5cd4884dc0a657262
thanhnm2 <manhthanh1997@gmail.com>
2023-01-03 12:21:43 +0700

    lan6

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,11 +1,13 @@
 const path = {
   home: '/',
   profile: '/profile',
+  user: '/user',
+  changePassword: '/user/password',
+  hitoryPurchase: '/user/purchase',
   login: '/login',
   register: '/register',
   logout: '/logout',
   productDetail: ':nameId',
   cart: '/cart'
 } as const
-
 export default path
